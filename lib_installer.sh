018aa505d5d8d181e5aa56913cc5296cb8f92043
Franciszek Cieślik <115489360+FranciszekCieslik@users.noreply.github.com>
2024-04-14 12:28:46 +0200

    Create lib_installer.sh

---
+++ lib_installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install PyQt6
+pip install numpy
+pip install scipy
+pip install opencv-python
+pip install torch
+pip install ultralytics
+pip install yolov8
+pip install face-recognition
